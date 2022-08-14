221f9b437f1cee502e8d88b843c58458b4ba6732
Oluseyi Olaleye <ictoluseyi@gmail.com>
2022-08-15 00:32:53 +0100

    starter files

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template
+from database import get_db
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite3_db'):
+        g.sqlite_db.close()    
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/question')
+def question():
+    return render_template('')
+
+@app.route('/answer')
+def answer():
+    return render_template('answer.html')
+
+@app.route('/ask')
+def ask():
+    return render_template('ask.html')
+
+@app.route('/unanswered')
+def unanswered():
+    return render_template('unanswered.html')
+
+@app.route('/users')
+def users():
+    return render_template('users.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
